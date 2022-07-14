db7bd867f3796769f29698b04635266582d18b62
Melak2017 <melakuandarge615@gmail.com>
2022-07-15 00:45:40 +0300

    fixied erros

--- location.sol
+++ location.sol
@@ -1,15 +1,16 @@
-pragma solidity ^0.5.16
+pragma solidity ^0.5.16;
 
-contract Locations{
-    string[2] cordinates = ["0.00","0.00"];
+contract locations {
+    string[2] coordinates = ["0.00","0.00"];
 
     function sendCoordinates(string memory _latitude,string memory _longitude) public{
         coordinates[0] = _latitude;
-        coordinates[1] = _longitude
+        coordinates[1] = _longitude;
     }
     
     function readCoordinate() view public returns(string memory, string memory){
-        returns (coordinates[0],coordinates[1]);
+        return (coordinates[0],coordinates[1]);
     }
 
 }
+
